c895f6b171c1c3598c170ccf8ed26fe21e56bc22
Vinicius Tonini <viniciusft81@gmail.com>
2024-04-10 14:39:03 -0300

    feat: implementado error_handler

--- src/main/routes/model_routes.py
+++ src/main/routes/model_routes.py
@@ -1,14 +1,20 @@
 from flask import Blueprint, request, jsonify
 from src.views.http_types.http_request import HttpRequest
 from src.views.img_received_view import ImgReceivedView
+from src.errors.error_handler import handle_errors
 
 model_routes_bp = Blueprint('model_routes', __name__)
 
 @model_routes_bp.route('/predict', methods=["POST"])
 def predict():
-    img_received_view = ImgReceivedView()
-    http_request = HttpRequest(body=request.json)
+    response = None
+    try:
+        img_received_view = ImgReceivedView()
     
-    response = img_received_view.validate_image(http_request)
+        http_request = HttpRequest(body=request.json)
+        response = img_received_view.validate_image(http_request)
+    
+    except Exception as e:
+        response = handle_errors(e)
     
     return jsonify(response.body), response.status_code
