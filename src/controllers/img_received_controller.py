3a8e2687abac81feff8a253810bceed073b346cb
Vinicius Tonini <viniciusft81@gmail.com>
2024-04-11 17:50:14 -0300

    feat: implementado modelo para classificar se a imagem recebida é planta

--- src/controllers/img_received_controller.py
+++ src/controllers/img_received_controller.py
@@ -5,6 +5,7 @@ from PIL import Image
 import dill 
 from fastai.vision.learner import load_learner
 from src.drivers.img_handler import ImgHandler
+from src.drivers.img_classification import img_classification
 
 class ImgReceivedController:
 	'''
@@ -27,17 +28,24 @@ class ImgReceivedController:
 			#transforma a imagem em um objeto PIL
 			img = Image.open(BytesIO(img_bytes))
 			print("Imagem decodificada com sucesso:", img)
-
-			resize_img = img.resize((460, 460))
-			#show_image(img)
-			#realiza a predição
-			pred_class, pred_idx, pred_outputs = self.__learn.predict(resize_img)
-			logging.info("Predição realizada com sucesso: %s, %s, %s", pred_class, pred_idx, pred_outputs)
    
-			prob_one, prob_two= float(pred_outputs[0]), float(pred_outputs[1])
-			logging.info("probs=> %s, %s", prob_one, prob_two)
+			classification = img_classification(img)
+
+			if classification:
+				
+				resize_img = img.resize((460, 460))
+				#realiza a predição
+				pred_class, pred_idx, pred_outputs = self.__learn.predict(resize_img)
+				logging.info("Predição realizada com sucesso: %s, %s, %s", pred_class, pred_idx, pred_outputs)
+   
+				prob_prague, prob_healthy = float(pred_outputs[0]), float(pred_outputs[1])
+				logging.info("probs=> %s, %s", prob_prague, prob_healthy)
 		
-			return self.__format_response(pred_class, prob_one, prob_two)
+				return self.__format_response(pred_class, prob_prague, prob_healthy)
+			else:
+				return {
+					"message": "Planta não identificada"
+				}
 		except Exception as e:
 			print(f"Erro ao realizar a predição: {e}")
 			return {
@@ -49,11 +57,9 @@ class ImgReceivedController:
 	def __load_model(self):
 		self.__learn = load_learner("model_soy.pkl", pickle_module=dill)
  	
-	def __format_response(self, prediction_class, prediction_prob1, prediction_prob2) -> Dict:
-		return [{
-			"data": {
-				"pred_class": str(prediction_class),
-				"pred_prob1": prediction_prob1, 
-    			"pred_prob2": prediction_prob2
-			}
-		}]
+	def __format_response(self, prediction_class, prediction_prob_prague, prediction_prob_healthy) -> Dict:
+		return {
+			"pred_class": str(prediction_class),
+			"prague_prob": prediction_prob_prague, 
+    		"healthy_prob": prediction_prob_healthy
+		}
---
+++ src/drivers/img_classification.py
@@ -0,0 +1,25 @@
+from ultralyticsplus import YOLO
+
+def img_classification(image):
+    # carrega modelo
+    model = YOLO('foduucom/plant-leaf-detection-and-classification')
+
+    # parâmetros do modelo
+    model.overrides['conf'] = 0.25  # NMS confidence threshold
+    model.overrides['iou'] = 0.45  # NMS IoU threshold
+    model.overrides['agnostic_nms'] = False  # NMS class-agnostic
+    model.overrides['max_det'] = 1000  # maximum number of detections per image
+
+    # perform inference
+    results = model.predict(image)
+    # observe results
+    print("=========================================")
+    result = results[0]
+    categories = result.boxes.cls
+    print("category", categories)
+    print("=========================================")
+    
+    if len(categories) > 0:
+        return True
+    else: return False
+        
