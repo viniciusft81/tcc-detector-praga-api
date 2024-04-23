0ba932febf32b2ded94c0cd1b26975462b47da4f
Vinicius Tonini <viniciusft81@gmail.com>
2024-04-22 21:45:16 -0300

    fix: ajuste no flask CORS

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 from src.main.routes.model_routes import model_routes_bp
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.register_blueprint(model_routes_bp)
